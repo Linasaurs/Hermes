ad808197248704e9f06599b04ae26f353d8facaf
Hussam A. El-Araby <hussam.a@aucegypt.edu>
2018-04-11 18:00:16 +0200

    updated arduino file to be with 2 ultrasonics

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -24,12 +24,12 @@ ros::NodeHandle  nh;
 
 std_msgs::String str_msg;
 ros::Publisher chatter("arduino", &str_msg);
-Ping ping = Ping(13,0,0);
+Ping s_TL = Ping(12,0,0);
+Ping s_BL = Ping(13,0,0);
 char buffer[80];
 
 void setup()
 {
-  //Serial.begin(9600);
   nh.getHardware()->setBaud(115200);
   nh.initNode();
   nh.advertise(chatter);
@@ -37,14 +37,21 @@ void setup()
 
 void loop()
 {
-  ping.fire();
-  int cm = ping.centimeters();
-  //Serial.print(cm);
-  //Serial.print("\n");
-  delay(100);
-  snprintf(buffer,sizeof(buffer),"%d",cm);
-  str_msg.data= buffer;
+  s_TL.fire();
+  int cm_TL = s_TL.centimeters();
+  snprintf(buffer,sizeof("TL "),"%s","TL ");
+  snprintf(&buffer[3],sizeof(buffer),"%d",cm_TL);
+  str_msg.data = buffer;
   chatter.publish( &str_msg );
+  
+  s_BL.fire();  
+  int cm_BL = s_BL.centimeters();  
+  snprintf(buffer,sizeof("BL "),"%s","BL ");
+  snprintf(&buffer[3],sizeof(buffer),"%d",cm_BL);
+  str_msg.data = buffer;
+  chatter.publish( &str_msg );
+  
+  delay(100);
   nh.spinOnce();
 }
 
