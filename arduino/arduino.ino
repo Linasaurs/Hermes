cc6f7f30109472c7f43a8a949ee65f93c8a1c7f8
Hussam A. El-Araby <hussam.a@aucegypt.edu>
2018-04-11 20:42:30 +0200

    .

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -19,22 +19,37 @@ significant bit) at this FS setting, so the raw reading of
 #include <std_msgs/String.h>
 #include <Ping.h>
 
+#include <Wire.h>
+#include <LSM6.h>
+
+LSM6 imu;
+
+
 ros::NodeHandle  nh;
 
 
 std_msgs::String str_msg;
 ros::Publisher pub_front("sonic_front", &str_msg);
 ros::Publisher pub_right("sonic_right", &str_msg);
+ros::Publisher imu_pub("imuArduino", &str_msg);
 Ping s_front = Ping(12,0,0);
 Ping s_right = Ping(13,0,0);
 char buffer[80];
 
 void setup()
 {
+  Wire.begin();
+
+  if (!imu.init())
+  {
+    while (1);
+  }
+  imu.enableDefault();  
   nh.getHardware()->setBaud(115200);
   nh.initNode();
   nh.advertise(pub_front);
-  nh.advertise(pub_right);  
+  nh.advertise(pub_right);
+  nh.advertise(imu_pub);  
 }
 
 void loop()
@@ -50,6 +65,14 @@ void loop()
   snprintf(buffer,sizeof(buffer),"%d",cm_right);
   str_msg.data = buffer;
   pub_right.publish( &str_msg );
+  
+  imu.read();
+
+  snprintf(buffer, sizeof(buffer), "%6d %6d %6d %6d %6d %6d",
+    imu.a.x, imu.a.y, imu.a.z,
+    imu.g.x, imu.g.y, imu.g.z);
+  str_msg.data = buffer;
+  imu_pub.publish( &str_msg );
   
   delay(100);
   nh.spinOnce();
