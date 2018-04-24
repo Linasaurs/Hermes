f43c46681bf97d4569dfd625c6c59f6dcebd5540
Hussam A. El-Araby <hussam.a@aucegypt.edu>
2018-04-24 06:14:38 -0400

    hi

--- startup.sh
+++ startup.sh
@@ -2,7 +2,10 @@
 export ROS_IP=$1
 export ROS_MASTER=$2
 export ROS_MASTER_URI=http://$ROS_MASTER:11311
+if [ $ROS_IP = $ROS_MASTER ]
+then
 gnome-terminal --window-with-profile=keep -e "roscore"
+fi
 sleep 2
 gnome-terminal --window-with-profile=keep -e "roslaunch jetson_csi_cam jetson_csi_cam.launch width:=640 height:=360 fps:=16"
 sleep 0.5
