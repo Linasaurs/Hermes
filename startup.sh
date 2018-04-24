9da288d024ae9cae9b353c5cf660f2882d6cc46e
Hussam A. El-Araby <hussam.a@aucegypt.edu>
2018-04-24 05:29:10 -0400

    hi

--- startup.sh
+++ startup.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
-roscore &
+trap "kill 0" EXIT
+gnome-terminal --window-with-profile=keep -e "roscore"
 sleep 2
-roslaunch jetson_csi_cam jetson_csi_cam.launch width:=640 height:=360 fps:=16 &
+gnome-terminal --window-with-profile=keep -e "roslaunch jetson_csi_cam jetson_csi_cam.launch width:=640 height:=360 fps:=16"
 sleep 0.5
-rosrun rosserial_python serial_node.py /dev/ttyACM0 _baud:=115200 &
+gnome-terminal --window-with-profile=keep -e "rosrun rosserial_python serial_node.py /dev/ttyACM0 _baud:=115200"
 sleep 0.5
-python ./miniproject_src/imu/imuArepublisher.py &
+gnome-terminal --window-with-profile=keep -e "rosrun traverse imu.py"
 sleep 0.5
-rosrun imu_filter_madgwick imu_filter_node _use_mag:=false &
+gnome-terminal --window-with-profile=keep -e "rosrun imu_filter_madgwick imu_filter_node _use_mag:=false"
 sleep 0.5
-python ./miniproject_src/imu/getyaw.py &
+gnome-terminal --window-with-profile=keep -e "rosrun traverse getyaw.py"
+#sleep 0.5
+#gnome-terminal --window-with-profile=keep -e python "./miniproject_src/vision/newvision2.py"
 sleep 0.5
-python ./miniproject_src/vision/newvision2.py &
+rosparam set /pid_node/Kp $1
+rosparam set /pid_node/Kd $2
 sleep 0.5
-python ./miniproject_src/lidar_distance/lidar_distance.py &
+gnome-terminal --window-with-profile=keep -e "rosrun pid controller"
 sleep 0.5
-rosparam set /pid_node/Kp 0.7 &
+gnome-terminal --window-with-profile=keep -e "rostopic pub /setpoint std_msgs/Float64 30"
 sleep 0.5
-rosrun pid controller &
-sleep 0.5
-rostopic pub /setpoint std_msgs/Float64 0 &
-sleep 0.5
-rosrun test_python control.py &
+gnome-terminal --window-with-profile=keep -e "rosrun test_python control.py"
 echo "!!! end of startup.sh"
+wait
