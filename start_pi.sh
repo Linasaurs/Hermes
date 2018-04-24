68918909d3f3b4d0af829e66d3dfa478b4d95cd5
Hussam A. El-Araby <hussam.a@aucegypt.edu>
2018-04-24 15:39:43 +0000

    hi

---
+++ start_pi.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+export ROS_IP=$1
+export ROS_MASTER=$2
+export ROS_MASTER_URI=http://$ROS_MASTER:11311
+if [ $ROS_IP = $ROS_MASTER ]
+then
+gnome-terminal --window-with-profile=keep -e "roscore"
+fi
+sleep 2
+gnome-terminal --window-with-profile=keep -e "rosrun rosserial_python serial_node.py /dev/ttyACM0 _baud:=115200"
+sleep 0.5
+gnome-terminal --window-with-profile=keep -e "rosrun traverse imu.py"
+sleep 0.5
+gnome-terminal --window-with-profile=keep -e "rosrun imu_filter_madgwick imu_filter_node _use_mag:=false"
+sleep 0.5
+gnome-terminal --window-with-profile=keep -e "rosrun traverse getyaw.py"
+#sleep 0.5
+#gnome-terminal --window-with-profile=keep -e python "./miniproject_src/vision/newvision2.py"
+sleep 0.5
+rosparam set /pid_node/Kp $3
+rosparam set /pid_node/Kd $4
+sleep 0.5
+gnome-terminal --window-with-profile=keep -e "rosrun pid controller"
+sleep 0.5
+gnome-terminal --window-with-profile=keep -e "rostopic pub /setpoint std_msgs/Float64 30"
+sleep 0.5
+gnome-terminal --window-with-profile=keep -e "rosrun traverse pid.py"
+sleep 0.5
+gnome-terminal --window-with-profile=keep -e "rosrun traverse control.py"
