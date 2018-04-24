33bc514f6c34e17e1f11e93c031428fa399a4fa9
Hussam A. El-Araby <hussam.a@aucegypt.edu>
2018-04-24 05:56:07 -0400

    hi

--- startup.sh
+++ startup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+export ROS_IP=$1
+export ROS_MASTER=$2
+export ROS_MASTER_URI=http://$ROS_MASTER:11311
 gnome-terminal --window-with-profile=keep -e "roscore"
 sleep 2
 gnome-terminal --window-with-profile=keep -e "roslaunch jetson_csi_cam jetson_csi_cam.launch width:=640 height:=360 fps:=16"
@@ -13,8 +16,8 @@ gnome-terminal --window-with-profile=keep -e "rosrun traverse getyaw.py"
 #sleep 0.5
 #gnome-terminal --window-with-profile=keep -e python "./miniproject_src/vision/newvision2.py"
 sleep 0.5
-rosparam set /pid_node/Kp $1
-rosparam set /pid_node/Kd $2
+rosparam set /pid_node/Kp $3
+rosparam set /pid_node/Kd $4
 sleep 0.5
 gnome-terminal --window-with-profile=keep -e "rosrun pid controller"
 sleep 0.5
