44d035a5b5ffcce8582e7c6c811de656bdade766
Hussam A. El-Araby <hussam.a@aucegypt.edu>
2018-05-12 02:33:16 -0400

    gg

--- catkin_ws/vision/scripts/face_detect.py
+++ catkin_ws/vision/scripts/face_detect.py
@@ -22,75 +22,68 @@ def convert(data):
 		global found_msg
 		if found_msg=="yes":
 			found_msg = "no"
-			global filename
-			bridge = CvBridge()
-			frame = bridge.imgmsg_to_cv2(data, "bgr8")		
-			frame = imutils.resize(frame, width=frameWidth)
-			test_img1 = frame;
-			#cv2.imshow('frame',frame)
-			#perform a prediction
-			#out = cv2.imwrite('capture.jpg', frame)
-			if (sys.argv[1] == "jetson"):
-				out = cv2.imwrite('/home/nvidia/catkin_ws/src/vision/scripts/capture.jpg', frame)
-				filename="/home/nvidia/catkin_ws/src/vision/scripts/capture.jpg"
-			elif (sys.argv[1] == "pi"):
-				out = cv2.imwrite('/home/raspi3/catkin_ws/src/vision/scripts/capture.jpg', frame)
-				filename="/home/raspi3/catkin_ws/src/vision/scripts/capture.jpg"
-			predicted_img1 = predict(test_img1)
+			labels_detected = {}
+			for i in range(32):			
+				bridge = CvBridge()
+				frame = bridge.imgmsg_to_cv2(data, "bgr8")		
+				frame = imutils.resize(frame, width=frameWidth)
+				test_img = frame;
+				#cv2.imshow('frame',frame)
+				#perform a prediction
+				#out = cv2.imwrite('capture.jpg', frame)
+				if (sys.argv[1] == "jetson"):
+					out = cv2.imwrite('/home/nvidia/catkin_ws/src/vision/scripts/capture.jpg', frame)
+					filename="/home/nvidia/catkin_ws/src/vision/scripts/capture.jpg"
+				elif (sys.argv[1] == "pi"):
+					out = cv2.imwrite('/home/raspi3/catkin_ws/src/vision/scripts/capture.jpg', frame)
+					filename="/home/raspi3/catkin_ws/src/vision/scripts/capture.jpg"
+			
+				#make a copy of the image as we don't want to chang original image
+				img = test_img.copy()
+				#detect face from the image
+				face, rect = detect_face(img)
 
+				#predict the image using our face recognizer
+				if not face is None:
+					label, confidence = face_recognizer.predict(face)
+
+					if (confidence > 75):
+						print ("Pushed Unknown", label, confidence)
+						if not "unknown" in labels_detected:
+							labels_detected["unknown"] = 1
+						else:
+							labels_detected["unknown"] += 1
+					else:
+						label_text = subjects[label]
+						# draw a rectangle around face detected
+						#draw_rectangle(img, rect)
+						# draw name of predicted person
+						#draw_text(img, label_text, rect[0], rect[1] - 5)
+						print ("Pushed",label_text)
+						#pub_face.publish(label_text)
+						if not label_text in labels_detected:
+							labels_detected[label_text] = 1
+						else:
+							labels_detected[label_text] += 1
+				else:
+					print ("Pushed No face detected")
+					if not "No face detected" in labels_detected:
+						labels_detected["No face detected"] = 1
+					else:
+						labels_detected["No face detected"] += 1
+					#pub_ocr.publish(filename)
+	
+			answer = max(labels_detected, key=labels_detected.get)
+	
+			if (answer == "No face detected"):
+				print ("Answer: No face detected")
+				pub_ocr.publish(filename)
+			else:
+				print ("Answer:",answer)
+				pub_face.publish(answer)			
 		
 	except CvBridgeError as e:
 		print(e)
-		
-def predict(test_img):
-	#make a copy of the image as we don't want to chang original image
-	img = test_img.copy()
-	#detect face from the image
-	face, rect = detect_face(img)
-
-	#predict the image using our face recognizer
-	labels_detected = {}
-	for i in range(20):
-		if not face is None:
-			label, confidence = face_recognizer.predict(face)
-
-			if (confidence > 75):
-				print ("Pushed Unknown", label, confidence)
-				if not "unknown" in labels_detected:
-					labels_detected["unknown"] = 1
-				else:
-					labels_detected["unknown"] += 1
-			else:
-				label_text = subjects[label]
-				# draw a rectangle around face detected
-				#draw_rectangle(img, rect)
-				# draw name of predicted person
-				#draw_text(img, label_text, rect[0], rect[1] - 5)
-				print ("Pushed",label_text)
-				#pub_face.publish(label_text)
-				if not label_text in labels_detected:
-					labels_detected[label_text] = 1
-				else:
-					labels_detected[label_text] += 1
-		else:
-			print ("Pushed No face detected")
-			if not "No face detected" in labels_detected:
-				labels_detected["No face detected"] = 1
-			else:
-				labels_detected["No face detected"] += 1
-			#pub_ocr.publish(filename)
-	
-	answer = max(labels_detected, key=labels_detected.get)
-	
-	if (answer == "No face detected"):
-		print ("Answer: No face detected")
-		pub_ocr.publish(filename)
-	else:
-		print ("Answer:",answer)
-		pub_face.publish(answer)
-				
-
-	return img
 	
 #def draw_text(img, text, x, y):
 #	1+1
@@ -117,7 +110,6 @@ def detect_face(img):
 
 found_msg = "None"
 frameWidth = 600
-filename = "None"
 
 subjects = ["", "lina", "obama", "mazhar"]
 face_recognizer = cv2.face.LBPHFaceRecognizer_create()
